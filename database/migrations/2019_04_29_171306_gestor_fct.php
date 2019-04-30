835420a4d917666981a386d2197b89d5d7a6bc23
ncristiansa <ncristiansalinasandia@gmail.com>
2019-04-30 23:07:14 +0200

    cambios en migrate

--- database/migrations/2019_04_29_171306_gestor_fct.php
+++ database/migrations/2019_04_29_171306_gestor_fct.php
@@ -19,6 +19,12 @@ class GestorFct extends Migration
             $table->String('Nombre');
             $table->String('Clave');
         });
+        Schema::create('acuerdo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('Fecha_alta');
+            $table->date('Acabada');
+            $table->date('Fin');
+        });
         Schema::create('empresa', function (Blueprint $table) {
             $table->increments('id');
             $table->String('Nombre');
@@ -28,6 +34,8 @@ class GestorFct extends Migration
             $table->String('Idiomas');
             $table->String('Horario');
             $table->String('Seguimiento');
+            $table->integer('acuerdo_id')->unsigned();
+            $table->foreign('acuerdo_id')->references('id')->on('acuerdo');
         });
         Schema::create('persona', function (Blueprint $table) {
             $table->increments('id');
@@ -35,6 +43,7 @@ class GestorFct extends Migration
             $table->integer("empresa_id")->unsigned();
             $table->foreign('empresa_id')->references('id')->on('empresa');
         });
+        
         Schema::create('alumno', function (Blueprint $table) {
             $table->increments('id');
             $table->String('Nom');
@@ -42,19 +51,18 @@ class GestorFct extends Migration
             $table->String('Num_CAP');
             $table->string('Email');
             $table->string('Telefono');
+            $table->integer('acuerdo_id')->unsigned();
+            $table->foreign('acuerdo_id')->references('id')->on('acuerdo');
         });
         Schema::create('tutor', function (Blueprint $table) {
             $table->increments('id');
             $table->String('Nombre');
             $table->String('Email');
             $table->String('Telefono');
+            $table->integer('acuerdo_id')->unsigned();
+            $table->foreign('acuerdo_id')->references('id')->on('acuerdo');
         });
-        Schema::create('acuerdo', function (Blueprint $table) {
-            $table->increments('id');
-            $table->date('Fecha_alta');
-            $table->date('Acabada');
-            $table->date('Fin');
-        });
+        
         Schema::create('seguimiento', function (Blueprint $table) {
             $table->increments('id');
             $table->String('Descripcion');
