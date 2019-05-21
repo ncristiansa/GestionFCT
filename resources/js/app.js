8985310eaffc7a598be0755577e61f97e5a9ef7b
ncristiansa <ncristiansalinasandia@gmail.com>
2019-05-21 20:18:07 +0200

    solucionar borrar alumno

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,10 +7,10 @@
 
 require('./bootstrap');
 require('../../public/js/script.js');
-require('../../public/js/modal-acuerdo.js');
 require('../../public/js/modal.js');
 require('../../public/js/modal-alumno.js');
-require('../../public/js/modal-tutor.js');
+
+
 
 window.Vue = require('vue');
 
